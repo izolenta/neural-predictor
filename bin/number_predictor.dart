4116468aeba6291d74373665f445103ba9defd4d
Alexey Sharov <alexey.sharov@team.wrike.com>
2020-12-03 21:41:09 +0300

    preparation

--- bin/number_predictor.dart
+++ bin/number_predictor.dart
@@ -5,38 +5,38 @@ import 'package:perceptron/perceptron.dart';
 
 void predictNumbers() {
   final data = [
-    TrainingData([0,0,0,0,0],[0]),
-    TrainingData([0,0,0,0,1],[0]),
-    TrainingData([0,0,0,1,0],[0]),
-    TrainingData([0,0,0,1,1],[0]),
-    TrainingData([0,0,1,0,0],[0]),
-    TrainingData([0,0,1,0,1],[0]),
-    TrainingData([0,0,1,1,0],[0]),
-    TrainingData([0,0,1,1,1],[1]),
-    TrainingData([0,1,0,0,0],[0]),
-    TrainingData([0,1,0,0,1],[0]),
-    TrainingData([0,1,0,1,0],[0]),
-    TrainingData([0,1,0,1,1],[1]),
-//    TrainingData([0,1,1,0,0],[0]),
-    TrainingData([0,1,1,0,1],[1]),
-    TrainingData([0,1,1,1,0],[1]),
-    TrainingData([0,1,1,1,1],[1]),
-    TrainingData([1,0,0,0,0],[0]),
-    TrainingData([1,0,0,0,1],[0]),
-//    TrainingData([1,0,0,1,0],[0]),
-    TrainingData([1,0,0,1,1],[1]),
-    TrainingData([1,0,1,0,0],[0]),
-//    TrainingData([1,0,1,0,1],[1]),
-    TrainingData([1,0,1,1,0],[1]),
-    TrainingData([1,0,1,1,1],[1]),
-//    TrainingData([1,1,0,0,0],[0]),
-    TrainingData([1,1,0,0,1],[1]),
-    TrainingData([1,1,0,1,0],[1]),
-    TrainingData([1,1,0,1,1],[1]),
-    TrainingData([1,1,1,0,0],[1]),
-    TrainingData([1,1,1,0,1],[1]),
-    TrainingData([1,1,1,1,0],[1]),
-    TrainingData([1,1,1,1,1],[1]),
+    TrainingData([0,0,0,0,0],[0]),  //1
+    TrainingData([0,0,0,0,1],[0]),  //2
+    TrainingData([0,0,0,1,0],[0]),  //3
+    TrainingData([0,0,0,1,1],[0]),  //4
+    TrainingData([0,0,1,0,0],[0]),  //5
+    TrainingData([0,0,1,0,1],[0]),  //6
+    TrainingData([0,0,1,1,0],[0]),  //7
+    TrainingData([0,0,1,1,1],[1]),  //8
+    TrainingData([0,1,0,0,0],[0]),  //9
+    TrainingData([0,1,0,0,1],[0]),  //10
+    TrainingData([0,1,0,1,0],[0]),  //11
+    TrainingData([0,1,0,1,1],[1]),  //12
+    TrainingData([0,1,1,0,0],[0]),  //13
+    TrainingData([0,1,1,0,1],[1]),  //14
+    TrainingData([0,1,1,1,0],[1]),  //15
+    TrainingData([0,1,1,1,1],[1]),  //16
+    TrainingData([1,0,0,0,0],[0]),  //17
+    TrainingData([1,0,0,0,1],[0]),  //18
+    TrainingData([1,0,0,1,0],[0]),  //19
+    TrainingData([1,0,0,1,1],[1]),  //20
+    TrainingData([1,0,1,0,0],[0]),  //21
+    TrainingData([1,0,1,0,1],[1]),  //22
+    TrainingData([1,0,1,1,0],[1]),  //23
+    TrainingData([1,0,1,1,1],[1]),  //24
+    TrainingData([1,1,0,0,0],[0]),  //25
+    TrainingData([1,1,0,0,1],[1]),  //26
+    TrainingData([1,1,0,1,0],[1]),  //27
+    TrainingData([1,1,0,1,1],[1]),  //28
+    TrainingData([1,1,1,0,0],[1]),  //29
+    TrainingData([1,1,1,0,1],[1]),  //30
+    TrainingData([1,1,1,1,0],[1]),  //31
+    TrainingData([1,1,1,1,1],[1]),  //32
   ];
 
   final perceptron = Perceptron([5, 4, 1], 1);
@@ -45,15 +45,14 @@ void predictNumbers() {
   }
   print('network trained');
   while (true) {
+    print('enter input data:');
     var line = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
     if (line == '\n') {
       break;
     }
     var input = line.split(' ').map((n) => double.parse(n)).toList();
-    print('Array $input: prediction: ${perceptron.process(input)}');
+    final result = perceptron.process(input);
+    final output = result.first < 0.1? '0 > 1' : result.first > 0.9? '1 > 0' : ' == ';
+    print('Array $input: prediction: $output (calculated value: $result)');
   }
-//  var input = <double>[1,0,1,0,1];
-//  print('Array $input: prediction: ${perceptron.process(input)}');
-//  input = <double>[0,1,1,0,0];
-//  print('Array $input: prediction: ${perceptron.process(input)}');
 }
